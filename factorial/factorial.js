15e0b47d0d81551a381d4a2d2265b0229825436c
ftc <northantsvintage@gmail.com>
2018-02-20 12:12:02 +0000

    updated js

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,12 +1,12 @@
 function firstFactorial(num) {
   var fact = 1;
   for (var i = 1; i <= num; i++) {
-    fact = num * i;
+    fact = fact * i;
   }
   return fact;
 
 }
 
 // keep this function call here
-var result = firstFactorial(32);
+var result = firstFactorial(5);
 console.log(result);
